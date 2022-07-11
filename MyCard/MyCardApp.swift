700a52334863b3511949d06f809422ad668094d4
PanMax13 <panezha-max@yandex.ru>
2022-07-11 14:30:12 +0000

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Максим Панежа on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
